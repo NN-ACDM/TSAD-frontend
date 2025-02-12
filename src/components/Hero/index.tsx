20061759177f7741950accdae62a8f9a53fff845
NN-ACDM <nn.acdm@gmail.com>
2025-02-12 10:03:17 +0700

    [logo] add

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <>
       <section
         id="home"
-        className="relative overflow-hidden bg-primary pt-[120px] md:pt-[130px] lg:pt-[160px]"
+        className="relative overflow-hidden bg-green pt-[120px] md:pt-[130px] lg:pt-[160px]"
       >
         <div className="container">
           <div className="-mx-4 flex flex-wrap items-center">
@@ -15,8 +15,11 @@ const Hero = () => {
                 className="hero-content wow fadeInUp mx-auto max-w-[780px] text-center"
                 data-wow-delay=".2s"
               >
+                <p className="mx-auto max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
+                  Welcome to
+                </p>
                 <h1 className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
-                  Open-source SaaS Starter Kit and Boilerplate for Next.js
+                Thai Society for Affective Disorders
                 </h1>
                 <p className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
                   Next.js SaaS Boilerplate and Starter Kit designed and built
